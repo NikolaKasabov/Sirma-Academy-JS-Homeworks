47a54026431a9472e021b3a58ee801c9232f5ab5
NikolaKasabov <n_kasabov@mail.bg>
2023-09-13 04:11:11 +0300

    task 32 completed

---
+++ 3.Loops/32.js
@@ -0,0 +1,26 @@
+function f(n) {
+  for (let i = 1; i <= n; i++) {
+    let line = ' '.repeat(n - i);
+
+    for (let j = 1; j <= i; j++) {
+      line += '* ';
+    }
+
+    console.log(line);
+  }
+
+  for (let i = (n - 1); i > 0; i--) {
+    let line = ' '.repeat(n - i);
+
+    for (let j = i; j > 0; j--) {
+      line += '* ';
+    }
+
+    console.log(line);
+  }
+}
+
+f(1);
+f(2);
+f(3);
+f(4);
