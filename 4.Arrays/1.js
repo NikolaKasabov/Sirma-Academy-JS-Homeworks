5a7f72c13e2e57fffcd7015c9f2b34130c80c606
NikolaKasabov <n_kasabov@mail.bg>
2023-09-14 05:05:41 +0300

    task 1 completed

---
+++ 4.Arrays/1.js
@@ -0,0 +1,8 @@
+function f(arr) {
+  const sum = Number(arr[0]) + Number(arr[arr.length - 1]);
+  console.log(sum);
+}
+
+f(['20', '30', '40']);
+f(['10', '17', '22', '33']);
+f(['11', '58', '69']);
