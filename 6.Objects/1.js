4a3bce9e2b2e006f7410e631db547a3725965644
NikolaKasabov <n_kasabov@mail.bg>
2023-09-21 05:58:08 +0300

    task 1 completed

---
+++ 6.Objects/1.js
@@ -0,0 +1,10 @@
+function f(firstName, lastName, age) {
+  return {
+    firstName,
+    lastName,
+    age
+  }
+}
+
+console.log(f("Ivan", "Ivanov", 29));
+console.log(f("Maria", "Marinova", 13));
